d0b61a2c21d79deddbd1c4b74716695754915db3
Erica Thygesen <Erth1001@stud.ek.dk>
2026-09-25 08:52:27 +0200

    klikbare elementer

---
+++ produktliste.js
@@ -0,0 +1,25 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/products";
+
+const productid = 123456;
+const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${productid}.webp`;
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then(visData);
+
+function visData(json) {
+  console.log(json);
+  json.forEach((element) => {
+    produktliste.innerHTML += `<a class="card" href=produktdetaljer.html?id=${element.id}>
+        <article class="card">
+        <img src=https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp alt="produktbillede" />
+            <h2>${element.productdisplayname}</h2>
+            <h3>${element.brandname}</h3>
+            <p>${element.price}</p>
+            <p>${element.category}</p>
+        </article>
+        </a>`;
+  });
+}
+
+const produktliste = document.querySelector(".produktliste");
